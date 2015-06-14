c16c0f6973281c9dd621c0f1827824caeea1befa
Ivan Clint A. Pabelona <ipabelona@gmail.com>
2015-06-14 18:41:29 +0800

    My Online Resume

---
+++ index.php
@@ -0,0 +1,494 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Web Developer | Changing the world with code</title>
+
+    <!-- Bootstrap -->
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="/assets/css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body data-spy="scroll" data-target="#navbar-example">
+
+    
+
+    <nav class="navbar navbar-fixed-top navbar-default header" id="navbar-example">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="">Ivan Clint A. Pabelona</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          
+          
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="#about-me">About Me</a></li>
+            <li><a href="#skills">Skills</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#education">Education</a></li>
+            <li><a href="#recent-work">Recent Work</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+
+
+     
+    
+    <main class="container">
+
+      <div class="row">
+        <div class="col-xs-12 col-md-12">
+          <div class="text-right">
+            <a style="display: none" href=""><span class="glyphicon glyphicon-save"></span> Download PDF </a>
+          </div>
+        </div>
+      </div>
+
+      <br />
+
+      <div class="row" id="about-me">
+        <div class="col-xs-12 col-md-3">
+          <a class="thumbnail mypic">
+            <img src="/assets/images/me.jpg" alt="Picture of myself">
+          </a>
+        </div>
+        
+        <div class="col-xs-12 col-md-9">
+          <h2>Ivan Clint A. Pabelona</h2>
+          <h3 class="main-color my-role">Web Developer</h3>
+          
+          <div class="well desc">
+            <p>
+              I'm a freelance web developer based in the Philippines. I have been into web development for 6 years now. I'm using PHP, CSS, Javascript, JQuery, HTML, Bootstrap , CodeIgniter, Laravel and wordpress in developing softwares.
+            </p>
+            <p>
+              I work with a huge range of companies from personal bloggers to Councils. I'm a creative problem solver who is not afraid to push the envelope of my skill set and provide new solutions.
+            </p>
+            <p>
+              Outside of work I spend my time into my music playing the piano and guitar and also reading books.
+            </p>
+          </div>
+        </div>
+
+      </div>
+      <br />
+
+      <div class="row social-icons">
+        <div class="col-xs-12 col-md-12">
+            <p>Follow Me</p>
+            <div>
+              <a href="//facebook.com/ipabz" target="_blank">
+                <img src="/assets/images/fb.png" class="img-thumbnail" alt="My FB profile">
+              </a>
+              <a href="//plus.google.com/115124676079255286105" target="_blank">
+                <img src="/assets/images/g+64.png" class="img-thumbnail" alt="My Google Plus profile">
+              </a>
+              <a href="mailto:ipabelona@gmail.com" target="_blank">
+                <img src="/assets/images/email.png" class="img-thumbnail" alt="Email Me">
+              </a>
+              <a href="//github.com/ipabz" target="_blank">
+                <img src="/assets/images/GitHub_Logo.png" class="img-thumbnail" alt="My GitHub Profile">
+              </a>
+              
+
+            </div>
+
+        </div>
+      </div>
+
+      <br /><br />
+
+      <div class="row" id="skills">
+
+        <div class="col-xs-12 col-md-12">
+
+          <h2><span class="glyphicon glyphicon-cog"></span> Skills</h2>
+          <br />
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-12 col-md-2"><h4>Languages</h4></div>
+              <div class="col-xs-12 col-md-10">
+                <h5>
+                  <a class="label label-default">PHP</a>
+                  <a class="label label-default">MySQL</a>
+                  <a class="label label-default">Java</a>
+                  <a class="label label-default">Javascript</a>
+                  <a class="label label-default">JQuery</a>
+                  <a class="label label-default">AJAX</a>
+                  <a class="label label-default">HTML</a>
+                  <a class="label label-default">HTML 5</a>
+                  <a class="label label-default">CSS</a>
+                  <a class="label label-default">CSS3</a>
+                  <a class="label label-default">JSON</a>
+                  <a class="label label-default">XML</a>
+                </h5>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-xs-12 col-md-2"><h4>CMS</h4></div>
+              <div class="col-xs-12 col-md-10">
+                <h5>
+                  <a class="label label-default">Wordpress</a>
+                  <a class="label label-default">Joomla</a>
+                  <a class="label label-default">Zendcart</a>
+                  
+                </h5>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-xs-12 col-md-2"><h4>Frameworks</h4></div>
+              <div class="col-xs-12 col-md-10">
+                <h5>
+                  <a class="label label-default">CodeIgniter</a>
+                  <a class="label label-default">Laravel</a>
+                  <a class="label label-default">Symfony</a>
+                  
+                </h5>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-xs-12 col-md-2"><h4>Softwares</h4></div>
+              <div class="col-xs-12 col-md-10">
+                <h5>
+                  <a class="label label-default">Sublime Text 2</a>
+                  <a class="label label-default">Netbeans</a>
+                  <a class="label label-default">Adobe Dream Weaver</a>
+                  <a class="label label-default">Adobe Fireworks</a>
+                  <a class="label label-default">Adobe Photoshop</a>
+                  <a class="label label-default">Navicat</a>
+                  <a class="label label-default">MySQL Workbench</a>
+                  <a class="label label-default">MS Office</a>
+                  <a class="label label-default">Libre Office</a>
+                  <a class="label label-default">XAMPP</a>
+                  <a class="label label-default">LAMP</a>
+                  
+                </h5>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-xs-12 col-md-2"><h4>Operating Systems</h4></div>
+              <div class="col-xs-12 col-md-10">
+                <h5>
+                  <a class="label label-default">Windows</a>
+                  <a class="label label-default">Linux</a>
+                  <a class="label label-default">Android</a>
+                  
+                </h5>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-xs-12 col-md-2"><h4>Social Platforms</h4></div>
+              <div class="col-xs-12 col-md-10">
+                <h5>
+                  <a class="label label-default">Social APIs</a>
+                  
+                </h5>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-xs-12 col-md-2"><h4>Payment Platforms</h4></div>
+              <div class="col-xs-12 col-md-10">
+                <h5>
+                  <a class="label label-default">Stripe</a>
+                  <a class="label label-default">Paypal</a>
+                  
+                </h5>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-xs-12 col-md-2"><h4>Content Marketing</h4></div>
+              <div class="col-xs-12 col-md-10">
+                <h5>
+                  <a class="label label-default">Search Engine Optimization</a>
+                  
+                </h5>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-xs-12 col-md-2"><h4>IT Operations</h4></div>
+              <div class="col-xs-12 col-md-10">
+                <h5>
+                  <a class="label label-default">Server Administration</a>
+                  
+                </h5>
+              </div>
+            </div>
+
+
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <br /><br />
+
+      <div class="row" id="experience">
+
+        <div class="col-xs-12 col-md-12">
+
+          <h2><span class="glyphicon glyphicon-calendar"></span> Experience</h2>
+          <br />
+
+          <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingOne">
+                <h4 class="panel-title">
+                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    May 2015 to Present - Web Developer @ Squiggle Group
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                <div class="panel-body">
+                  <p>
+                    Responsible for developing web apps that are needed by the company.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingTwo">
+                <h4 class="panel-title">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    2013 to 2015 - Web Developer @ Rise Media Group, LLC
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                <div class="panel-body">
+                  <div>Responsibilities</div>
+                  <p>
+                    <ul>
+                      <li>PHP programming ­ Maintaining and creating applications, api integrations and databases</li>
+                      <li>Urgent bug fixes and data recovery / restoration</li>
+                      <li>Linux server maintenance</li>
+                      <li>Code refactoring</li>
+                      <li>Front end development - html, css, js</li>
+                      <li>Testing all code committed</li>
+                    </ul>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingThree">
+                <h4 class="panel-title">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    2010 to 2013 - WEB & Application Programmer @ Suns Heavy Industries Limited
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                <div class="panel-body">
+                  <div>What I did</div>
+                  <p>
+                    <ul>
+                      <li>PHP programming ­ Maintaining and creating applications, api integrations and databases</li>
+                      <li>Urgent bug fixes and data recovery / restoration</li>
+                      <li>Built a customized shopping cart based from Zendcart </li>
+                      <li>Built an online booking system from scratch using codeigniter framework</li>
+                      <li>Built a web based POS using codeigniter framework</li>
+                      <li>Built Internet cafe management system</li>
+                      <li>Built an Android APP that prevents user without tokens from using an android device use in internet cafe's</li>
+                      <li>Built a web based Restaurant Management System using codeigniter framework.</li>
+                      <li>Built an online network marketing software</li>
+                    </ul>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingThree">
+                <h4 class="panel-title">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    2008 to 2009 – WEB & Application programmer @ Trisun Film, Dumaguete city.
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                <div class="panel-body">
+                  <div>What I did</div>
+                  <p>
+                    <ul>
+                      <li>PHP programming ­ Maintaining and creating applications, api integrations and databases</li>
+                      <li>Built a desktop based POS written in Java</li>
+                    </ul>
+                  </p>
+                </div>
+              </div>
+            </div>
+
+
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <br /><br />
+
+      <div class="row" id="education">
+
+        <div class="col-xs-12 col-md-12">
+
+          <h2><span class="glyphicon glyphicon-bookmark"></span> Education</h2>
+          <br />
+
+          <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="headingOne">
+                <h4 class="panel-title">
+                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    Foundation University
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                <div class="panel-body">
+                  
+                  <div>
+
+                    <p><small class="muted">from 2005 to 2012</small></p>
+
+                    <h4 class="media-heading primary-color">Bachelor of Science in Computer Science </h4>
+
+                    <p>I Have learned a lot from this University. It helped me built the basic skills I needed in the computer programming world.</p>  
+
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+      <br /><br />
+
+      <div class="row" id="recent-work">
+
+        <div class="col-xs-12 col-md-12">
+
+          <h2><span class="glyphicon glyphicon-bookmark"></span> Recent Work</h2>
+          <br />
+
+          <div class="row">
+            <div class="col-xs-6 col-md-3">
+              <a href="//factorydirect.net.au/" target="_blank" class="thumbnail">
+                <img src="/assets/images/vamba.png" alt="Collaboration Portal">
+              </a>
+            </div>
+
+            <div class="col-xs-6 col-md-3">
+              <a href="//mpr.directbooks.biz/" target="_blank" class="thumbnail">
+                <img src="/assets/images/games.png" alt="Hawaii VA Games">
+              </a>
+            </div>
+
+            <div class="col-xs-6 col-md-3">
+              <a target="_blank" class="thumbnail">
+                <img src="/assets/images/learn.png" alt="Learn Hawaii VA Loans Landing Pages">
+              </a>
+            </div>
+
+            <div class="col-xs-6 col-md-3">
+              <a target="_blank" class="thumbnail">
+                <img src="/assets/images/summer.png" alt="Summer Summit">
+              </a>
+            </div>
+            
+          </div>
+
+          <div class="row">
+            <div class="col-xs-6 col-md-3">
+              <a href="//factorydirect.net.au/" target="_blank" class="thumbnail">
+                <img src="/assets/images/factorydirect.png" alt="Factory Direct .Net .Au">
+              </a>
+            </div>
+
+            <div class="col-xs-6 col-md-3">
+              <a href="//mpr.directbooks.biz/" target="_blank" class="thumbnail">
+                <img src="/assets/images/mpr.png" alt="My Place Rental">
+              </a>
+            </div>
+
+            <div class="col-xs-6 col-md-3">
+              <a target="_blank" class="thumbnail">
+                <img src="/assets/images/restaurant.png" alt="Restaurant Management System">
+              </a>
+            </div>
+
+            <div class="col-xs-6 col-md-3">
+              <a target="_blank" class="thumbnail">
+                <img src="/assets/images/duck.png" alt="Duck and Bear POS">
+              </a>
+            </div>
+            
+          </div>
+
+          <div class="row">
+            <div class="col-xs-6 col-md-3">
+              <a href="//factorydirect.net.au/" target="_blank" class="thumbnail">
+                <img src="/assets/images/dtr.png" alt="Online DTR">
+              </a>
+            </div>
+
+            <div class="col-xs-6 col-md-3">
+              <a href="//mpr.directbooks.biz/" target="_blank" class="thumbnail">
+                <img src="/assets/images/mmp.png" alt="My Mobile Patron">
+              </a>
+            </div>
+
+            
+            
+          </div>
+
+        </div>
+      </div>
+
+      <br />
+
+
+
+
+    </main>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="/assets/js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="/assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
